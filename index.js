84ebbe03c49366166a715e7f04bac026f6406e22
Jason Bracco <bracco.jason@gmail.com>
2022-05-01 12:17:54 -0400

    commit tree 97abc25cb9638c2eb02352674d226c7b617f4268

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(){
+    return cats.push('Ralph')
+}
+
+function destructivelyPrependCat(){
+    return cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop(cats)
+    
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift(cats)
+}
+
+function appendCat(name){
+    const appendCat = [...cats, 'Broom' ]
+    return appendCat
+}
+
+function prependCat(name){
+    const prependCat = ['Arnold',...cats]
+    return prependCat
+}
+
+function removeLastCat(){
+    const removeLastCat = cats.slice(0,2)
+    return removeLastCat
+}
+
+function removeFirstCat(){
+    const removeFirstCat = cats.slice(-2)
+    return removeFirstCat
+}
